d7cb646fee55e7532460d68cb454bf94802c264b
Jaydeep davda <100603781+jaydeep-davda@users.noreply.github.com>
2026-01-29 17:29:06 +0100

    Refactor application.py to restore imports and functionality

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 #Import libraries
-pip install -r requirements.txt
 import pandas as pd
 import streamlit as st
 import seaborn as sns
@@ -174,4 +173,5 @@ st.plotly_chart(fig10, use_container_width=True)
 st.subheader("📋 Filtered Dataset Preview")
 
 st.dataframe(filtered_df.head(50))
+
 
